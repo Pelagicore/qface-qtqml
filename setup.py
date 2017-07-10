38df6ebceda536094fdf1fac1e9328f28dcbf1d1
Juergen Bocklage-Ryannel <juergen.bocklage-ryannel@pelagicore.com>
2017-07-10 16:41:56 +0200

    Added qtqml executable

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qface-qtqml',
+    version='1.0',
+    description='Qt QML generator based on the QFace library',
+    url='https://github.com/Pelagicore/qface-qtqml',
+    author='jryannel',
+    author_email='juergen@ryannel.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='qt code generator framework',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'qface',
+    ],
+    entry_points={
+        'console_scripts': [
+            'qtqml = qtqml:app'
+        ],
+    },
+)
